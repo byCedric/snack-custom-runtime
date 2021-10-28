b930c18ec5015c44a45c24489884e25277c3715b
Cedric van Putten <me@bycedric.com>
2021-10-28 18:33:28 +0200

    feature: add snack-runtime with default snack

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Snack from 'snack-runtime';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Snack snackUrl='exp://exp.host/@bycedric/great-bagel+REEOUkskIw' />
     </View>
   );
 }
@@ -15,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
