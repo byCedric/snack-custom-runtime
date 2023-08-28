7218782e09fae3c02f8024a646b2d44d5c448690
Cedric van Putten <github@cedric.dev>
2023-08-28 17:48:09 +0200

    chore: upgrade to `expo@48` and `snack-runtime@0.3.0-rc.1`

--- scripts/reanimated.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const reanimated = path.resolve(__dirname, '../node_modules/react-native-reanimated');
-
-// Copy the plugin to `plugin-standalone.js` to include it in the runtime
-fs.copyFileSync(path.join(reanimated, 'plugin.js'), path.join(reanimated, 'plugin-standalone.js'));
